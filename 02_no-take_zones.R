ca2d2b1f7607c67a486bca051646ce219d2857c3
lisefourniercarnoy <lisefourniercarnoy@gmail.com>
2026-01-22 23:44:22 +0800

    making the temporal and spatial closure references more flexible

--- 02_no-take_zones.R
+++ 02_no-take_zones.R
@@ -26,26 +26,30 @@ colour_palette <- eval(parse(text = readLines("yijarup_chapter_colours.txt")))
 common_crs = 4283
 bbox <- st_bbox(c(xmin = 114.4, xmax = 116.0, ymin = -35.3, ymax = -32.5), crs = common_crs)
 
+
 ## Files used in this script --------------------------------------------------
 
-file_NTZ    <- "data/output_data/01_B_wadandi_NTZ.shp"
+file_NTZ    <- "data/output_data/Q_NTZ_manual_clean_01_B.shp"
 file_water  <- "data/output_data/01_B_water.rds"
+
+fleets <- c("commercial", "boat_rec", "shore_rec") # these are the fleets from 01_B "MP_permissions" that describe fishing per marine park zone
+
 
 ## Load files -----------------------------------------------------------------
 
 NTZ <-  st_read(file_NTZ) %>%
   st_transform(common_crs) %>%
   mutate(
-    restriction_date_SC = case_when(name == "Rottnest"           ~ "01/07/2007", # add when exactly each NTZ was put in place
-                                 name == "Shoalwater Islands" ~ "01/01/2007",
-                                 name == "Marmion"            ~ "30/01/1992",
-                                 name == "Two Rocks"          ~ "01/07/2018",
-                                 name == "Ngari Capes"        ~ "10/04/2019",
-                                 name == "Geographe"          ~ "01/07/2018",
-                                 name == "South-west Corner"  ~ "01/07/2018",
-                                 .default = NULL # others aren't in the final grid
-                                 ),
-    restriction_date_SC = as.list(restriction_date_SC)
+    SC_restriction_date = case_when(name == "Rottnest"           ~ "01/07/2007", # add when exactly each NTZ was put in place
+                                    name == "Shoalwater Islands" ~ "01/01/2007",
+                                    name == "Marmion"            ~ "30/01/1992",
+                                    name == "Two Rocks"          ~ "01/07/2018",
+                                    name == "Ngari Capes"        ~ "10/04/2019",
+                                    name == "Geographe"          ~ "01/07/2018",
+                                    name == "South-west Corner"  ~ "01/07/2018",
+                                    .default = NULL # others aren't in the final grid
+    ),
+    SC_restriction_date = as.list(SC_restriction_date)
   ) %>% 
   st_make_valid()
 plot(NTZ$geometry)
@@ -55,9 +59,11 @@ water <- readRDS(file_water) %>%
   st_make_valid()
 plot(water)
 
-## make the cockburn sound pink snapper closure -------------------------------
 
-test <- matrix(c(
+## Make a temporal closure area -----------------------------------------------
+
+# TC = temporal closure
+TC_poly <- matrix(c(
   115.749655, -32.073611, # NE point
   115.608333, -32.073611, # NW point
   115.652778, -32.380851, # SW point
@@ -69,32 +75,32 @@ ncol = 2, byrow = TRUE,
 dimnames = list(NULL, c("lon", "lat")))
 
 pts <- st_as_sf(
-  data.frame(lon = test[,1], lat = test[,2]),
+  data.frame(lon = TC_poly[,1], lat = TC_poly[,2]),
   coords = c("lon", "lat"),
   crs = 4326
 )
-cs_closure <- st_convex_hull(st_union(pts))
-cs_closure_proj <- st_as_sf(st_transform(cs_closure, st_crs(water)))
+TC <- st_convex_hull(st_union(pts))
+TC <- st_as_sf(st_transform(TC, st_crs(water)))
 
 sf::sf_use_s2(FALSE)
-cs_closure_proj <- st_intersection(cs_closure_proj, st_make_valid(st_union(water)))
+TC <- st_intersection(TC, st_make_valid(st_union(water)))
 
-plot(cs_closure_proj, col = NA, border = "red", lwd = 2); plot(water$geometry, add = T)
+plot(TC, col = NA, border = "red", lwd = 2); plot(water$geometry, add = T)
 
 # below is a list and description of temporal restrictions. i've made it this way because it's easier to see and modify in the code, in case there are more temporal closures or more complex ones.
-temporal_closures = list(
+temporal_closure_details = list(
 #   YEAR,   MONTHS,           PERCENT OF EACH MONTH THAT IS FISHED
   c("2000", "9-10",           "0.5-0"),
   c("2005", "10-11-12",       "0-0-0.5"),
   c("2023", "8-9-10-11-12-1", "0-0-0-0-0-0")
 )
 
-cs_closure_proj <- cs_closure_proj %>% 
+TC <- TC %>% 
   mutate(
-    name = "Cockburn Sound temporal closure",
-    restriction_date_TC = list(as.integer(vapply(temporal_closures, `[`, "", 1))),
-    restriction_date_TC_months = list(vapply(temporal_closures, `[`, "", 2)),
-    restriction_date_TC_months_perc_fished = list(vapply(temporal_closures, `[`, "", 3))
+    name = "Cockburn Sound temporal closure", # WHATEVER NAME IS HERE SHOULD HAVE THE EXACT TERMS 'temporal closure' IN IT OR THE REST WONT WORK
+    TC_restriction_date = list(as.integer(vapply(temporal_closure_details, `[`, "", 1))),
+    TC_restriction_months = list(vapply(temporal_closure_details, `[`, "", 2)),
+    TC_restriction_date_perc_fished = list(vapply(temporal_closure_details, `[`, "", 3))
   ) %>% 
   rename(
     geometry = x
@@ -104,12 +110,12 @@ cs_closure_proj <- cs_closure_proj %>%
 # 2005: 01oct - 15dec source: https://www.wa.gov.au/government/media-statements/Gallop%20Labor%20Government/Spawning-closures-protect-Perth%27s-pink-snapper-20050819
 # 2023: 01aug - 31jan, source: https://www.wa.gov.au/government/announcements/recreational-demersal-fishing-closure-aid-stock-recovery
 
-# closure file
-NTZ <- bind_rows(cs_closure_proj, NTZ)
+# add the closures in the NTZ dataframe
+NTZ <- bind_rows(TC, NTZ)
 plot(NTZ[, !sapply(NTZ, is.list)])
 
 
-## Adjusting the grid to account for NTZ --------------------------------------
+## Adjusting the grid to account for temporal/spatial closures ----------------
 
 # Make a grid for areas that are not fished
 NTZarea <- st_intersection(NTZ, water) %>% 
@@ -123,30 +129,27 @@ NTZ_union <- st_union(NTZarea) %>%
   st_transform(common_crs)
 plot(NTZ_union)
 
-Fished_area <- st_difference(water, NTZ_union) %>% 
+fished_area <- st_difference(water, NTZ_union) %>% 
   st_make_valid() %>%
   st_transform(common_crs)
-plot(Fished_area$geometry)
+plot(fished_area$geometry)
 
 # Put this back together with the fished area to create "water" again
-df_Fished_area <- st_sf(Fished_area) %>%
+fished_area <- st_sf(fished_area) %>%
   mutate(status = "Fished",
-         restriction_date_SC = NA,
-         restriction_date_TC = NA,
-         restriction_date_TC_months = NA,
-         restriction_date_TC_months_perc_fished = NA)
-plot(df_Fished_area)
+         SC_restriction_date = NA,
+         TC_restriction_date = NA,
+         TC_restriction_months = NA,
+         TC_restriction_date_perc_fished = NA)
+plot(fished_area$geometry)
+fished_area[,fleets] <- NA
 
-df_NTZ_union <- st_sf(NTZarea) %>%
-  mutate(status = ifelse(zone_type == "Special Purpose Zone (Shore-based Activities) (IUCN VI)", # for cells in this shore-activities-only zones...
-                         "NTZ_for_boat_only", "NTZ_for_shore_and_boat"), # ... boat fishing is not allowed
-         status = ifelse(name == "Cockburn Sound temporal closure", 
-                         "temporal_closure", status)) %>%
-  dplyr::select(names(df_Fished_area))
+NTZ_union <- st_sf(NTZarea) %>%
+  mutate(status = case_when(grepl("temporal closure", name, fixed = TRUE) ~ "TC",
+                            .default = "SC")) %>% 
+  dplyr::select(names(fished_area), all_of(fleets))
 
-names(df_Fished_area); names(df_NTZ_union)
-
-water <- rbind(df_NTZ_union, df_Fished_area)
+water <- rbind(NTZ_union, fished_area)
 plot(water$geometry)
 
 
@@ -160,10 +163,12 @@ ggplot(water) +
 water$ID <- 1:nrow(water)
 
 
-## find cockburn sound --------------------------------------------------------
+## Identify spawning ground ---------------------------------------------------
+
+# sg = spawning ground
 
 # make a rough polygon of cockburn + warnbro sound
-test <- matrix(c(
+sg_poly <- matrix(c(
   115.742245, -32.050302,  # freo
   115.635004, -32.066268,  # straggler_rock
   115.838059, -32.365724,   # point_kennedy
@@ -174,41 +179,34 @@ ncol = 2, byrow = TRUE,
 dimnames = list(NULL, c("lon", "lat")))
 
 pts <- st_as_sf(
-  data.frame(lon = test[,1], lat = test[,2]),
+  data.frame(lon = sg_poly[,1], lat = sg_poly[,2]),
   coords = c("lon", "lat"),
   crs = 4326
 )
-cs <- st_convex_hull(st_union(pts))
-cs_proj <- st_transform(cs, st_crs(water))
-plot(water$geometry); plot(cs_proj, col = NA, border = "red", lwd = 2, add = TRUE)
+sg <- st_convex_hull(st_union(pts))
+sg <- st_transform(sg, st_crs(water))
+plot(water$geometry); plot(sg, col = NA, border = "red", lwd = 2, add = TRUE)
 
 # find the cell centroid that are inside the cockburn polygon
 plot(st_centroid(water[, !sapply(water, is.list)]))
-inside <- st_within(st_centroid(water), cs_proj)
-cs_cell_id <- which(lengths(inside) > 0)
+inside <- st_within(st_centroid(water), sg)
+sg_cell_id <- which(lengths(inside) > 0)
 
-intersections <- st_within(st_centroid(water$geometry), cs_proj)
+intersections <- st_within(st_centroid(water$geometry), sg)
 intersects_logical <- lengths(intersections) > 0
 
-cockburn_cells <- water[intersects_logical, ]
-plot(cockburn_cells[, !sapply(cockburn_cells, is.list)])
-cs_cell_id <- cockburn_cells$ID # find the cell IDs
-
-
-ggplot() + # plot check BEFORE adding cockburn type
-  geom_sf(data = water, aes(fill = type), col = NA) +
-  scale_fill_manual(values = colour_palette) +
-  theme_minimal()
+sg_cells <- water[intersects_logical, ]
+plot(sg_cells[, !sapply(sg_cells, is.list)])
+sg_cell_id <- sg_cells$ID # find the cell IDs
 
 # modify cockburn cells and change their type to identify them.
-water$type <- case_when(
-  water$ID %in% cs_cell_id & water$type == "shore_north" ~ "shore_north_cockburn_warnbro",
-  water$ID %in% cs_cell_id ~ "north_cockburn_warnbro",
-  .default = water$type
+water$spawning_status <- case_when(
+  water$ID %in% sg_cell_id ~ TRUE,
+  .default = FALSE
   )
 
-ggplot() + # plot check AFTER adding cockburn type
-  geom_sf(data = water, aes(fill = type), col = NA) +
+ggplot() + # plot check AFTER adding spawning type
+  geom_sf(data = water, aes(fill = spawning_status), col = NA) +
   scale_fill_manual(values = colour_palette) +
   theme_minimal()
 
@@ -221,31 +219,8 @@ water <- st_make_valid(water) %>%
   st_as_sf()
 
 
-# identify important cells
-
-# NTZ cells
-no_take_list_boat_shore <- water %>%
-  filter(status == "NTZ_for_boat_only" | status == "NTZ_for_shore_and_boat") %>%
-  st_drop_geometry() %>%
-  pull(ID) %>%
-  as.numeric()
-
-no_take_list_shore <- water %>%
-  filter(status == "NTZ_for_shore_and_boat") %>%
-  st_drop_geometry() %>%
-  pull(ID) %>%
-  as.numeric()
-
-
-# cockburn sound + warnbro sound cells
-cs_list <- water$ID[water$type %in% c("north_cockburn_warnbro", "shore_north_cockburn_warnbro")]
- 
-
 ## Save files for next step ---------------------------------------------------
 
-saveRDS(no_take_list_boat_shore, file = "data/output_data/02_no_take_list_boat_shore.rds")
-saveRDS(no_take_list_shore, file = "data/output_data/02_no_take_list_shore.rds")
-saveRDS(cs_cell_id, "data/output_data/02_cockburn_cell_id.rds")
 saveRDS(st_as_sf(water), file = "data/output_data/02_watergrid.rds")
 
 ### END ###
